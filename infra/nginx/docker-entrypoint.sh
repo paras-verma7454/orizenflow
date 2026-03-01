e3d26836eae58013f98d2d5cd76167d6f2778aa5
Paras verma <parasverma7454@gmail.com>
2026-03-01 12:25:33 +0530

    refactor: remove unused nginx configuration and related files

--- infra/nginx/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-# Block nginx from auto-generating default configs
-rm -f /etc/nginx/conf.d/default.conf~
-touch /etc/nginx/conf.d/.placeholder
-
-# Run original entrypoint but skip auto-config generation
-/docker-entrypoint.sh /bin/sh -c 'exec nginx -g "daemon off;"'
